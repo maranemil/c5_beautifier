e2b31a7b484de6eab4e70941c83731915a01aacf
Emil Maran <maran.emil@gmail.com>
2022-02-06 20:10:30 +0100

    formatted

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ c5_beautifier
 CodeMirror syntax highlighter for textarea working together with tinyMCE in Concrete5
 
 ## Info
+~~~
 * ** Author: Emil Maran
 * ** Tags: CodeMirror, syntax, highlighter
 * ** created:  2013 Oct 20
 * ** modified: 2013 Nov 19
 * ** Video Demo on Youtube: (http://www.youtube.com/watch?v=M9an55dDQ-w)
+~~~
+
 
 [![Editor Screen](https://raw.github.com/maranemil/c5_beautifier/master/editor_screen.png)](#features)
--- controller.php
+++ controller.php
@@ -1,56 +1,68 @@
-<?php
+<?php /** @noinspection PhpUnusedLocalVariableInspection */
+/** @noinspection PhpUndefinedConstantInspection */
+/** @noinspection PhpUndefinedFunctionInspection */
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection AutoloadingIssuesInspection */
+/** @noinspection PhpUnused */
 defined('C5_EXECUTE') or die("Access Denied.");
 
 /**
  * Class C5BeautifierPackage
+ * @method configurePackage(C5BeautifierPackage $pkg)
  */
-class C5BeautifierPackage extends Package {
+class C5BeautifierPackage extends Package
+{
 
-   protected $pkgHandle          = 'c5_beautifier';
-   protected $appVersionRequired = "5.4.1";
-   protected $pkgVersion         = "0.1.1";
+    protected $pkgHandle = 'c5_beautifier';
+    protected $appVersionRequired = "5.4.1";
+    protected $pkgVersion = "0.1.1";
 
-   public function getPackageDescription() {
-	  return t("CodeMirror syntax highlighter for textarea working together with tinyMCE in Concrete5 ");
-   }
+    public function getPackageDescription()
+    {
+        return t("CodeMirror syntax highlighter for textarea working together with tinyMCE in Concrete5 ");
+    }
 
-   public function GetPackageName() {
-	  return t("C5 syntax highlighter based on CodeMirror v0.1");
-   }
+    public function GetPackageName()
+    {
+        return t("C5 syntax highlighter based on CodeMirror v0.1");
+    }
 
-   public function install() {
-	  $pkg = parent::install();
+    public function install()
+    {
+        $pkg = parent::install();
 
-	  /* @var $fh FileHelper */
-	  $fh = Loader::helper('file');
+        /* @var $fh FileHelper */
+        $fh = Loader::helper('file');
 
-	  $sourceCSS = DIR_PACKAGES . "/" . $this->pkgHandle . "/css/";
-	  $targetCSS = DIR_BASE . "/css/";
-	  $fh->copyAll($sourceCSS, $targetCSS, $mode = null); // @ $mode = 0777
+        $sourceCSS = DIR_PACKAGES . "/" . $this->pkgHandle . "/css/";
+        $targetCSS = DIR_BASE . "/css/";
+        $fh->copyAll($sourceCSS, $targetCSS, $mode = null); // @ $mode = 0777
 
-	  $sourceJS = DIR_PACKAGES . "/" . $this->pkgHandle . "/js/.";
-	  $targetJS = DIR_BASE . "/js/";
-	  $fh->copyAll($sourceJS, $targetJS, $mode = null);
-   }
+        $sourceJS = DIR_PACKAGES . "/" . $this->pkgHandle . "/js/.";
+        $targetJS = DIR_BASE . "/js/";
+        $fh->copyAll($sourceJS, $targetJS,  null);
+    }
 
-   public function uninstall() {
-	  parent::uninstall();
+    public function uninstall()
+    {
+        parent::uninstall();
 
-	  /* @var $fh FileHelper */
-	  $fh = Loader::helper('file');
+        /* @var $fh FileHelper */
+        $fh = Loader::helper('file');
 
-	  $targetJS = DIR_BASE . "/js/";
-	  $fh->removeAll($targetJS);
+        $targetJS = DIR_BASE . "/js/";
+        $fh->removeAll($targetJS);
 
-	  $targetCSS = DIR_BASE . "/css/";
-	  $fh->removeAll($targetCSS);
-   }
+        $targetCSS = DIR_BASE . "/css/";
+        $fh->removeAll($targetCSS);
+    }
 
-   public function upgrade() {
-	  $pkg = $this;
-	  parent::upgrade();
-	  $this->configurePackage($pkg);
-   }
+    public function upgrade()
+    {
+        $pkg = $this;
+        parent::upgrade();
+        $this->configurePackage($pkg);
+    }
 }
 
 // vim: set noexpandtab ts=4 :
